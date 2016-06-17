ba63e27ac188e5d2f11cb2d47312f9f8a47acc73
Louise Deason <louise.deason@gmail.com>
2016-06-17 16:09:07 +0100

    Switched | for or because derp

--- #Problem1.py
+++ #Problem1.py
@@ -3,8 +3,8 @@
 
 def natural_num():
     total = 0
-    for num in range(1,1000):
-        if (num % 3 == 0 | num % 5 == 0):
+    for num in range(1000):
+        if (num % 3 == 0 or num % 5 == 0):
             total += num
     return total
 
